a8c730ed60ed0d758b18a83f6be33f5564153df6
Cao Xiaoqi <xqcao@thoughtworks.com>
2019-03-03 20:56:48 +0800

    Update ci config

--- config/jest.config.js
+++ config/jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   testPathIgnorePatterns: [
     '<rootDir>/src/index.js'
   ],
+  setupFilesAfterEnv: ['<rootDir>/config/__init__/setupTests.js'],
   coverageDirectory: '<rootDir>/coverage',
   coverageReporters: ['html'],
   collectCoverageFrom: [
@@ -17,7 +18,6 @@ module.exports = {
     '<rootDir>/coverage',
     '<rootDir>/node_modules'
   ],
-  setupFilesAfterEnv: ['<rootDir>/config/__init__/setupTests.js'],
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/__init__/fileMock.js'
