e83c595a57439740ab94ca085e1577caccb31aee
Xiaoqi Cao <xqcao@thoughtworks.com>
2019-06-20 14:33:18 +0800

    Add a parameter for set base url

--- src/EventHolder.js
+++ src/EventHolder.js
@@ -4,14 +4,20 @@ import Mixspa from '@mixspa/core';
 import { withRouter } from 'react-router';
 
 class EventHolder extends React.Component {
+  static defaultProps = {
+    basename: ''
+  };
   static propTypes = {
     history: PropTypes.object,
+    basename: PropTypes.string,
     children: PropTypes.node
   };
 
   componentDidMount() {
     Mixspa.on('mixspa:url:changed', (url) => {
-      this.props.history.push(url);
+      if (url.startsWith(this.props.basename)) {
+        this.props.history.push(url.substring(this.props.basename.length));
+      }
     });
   }
 
